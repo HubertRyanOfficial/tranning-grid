8b6d21ee7b77acf1f11f01f6207e2e4f12cc4b91
Hubert Ryan <hubertryanofficial@gmail.com>
2024-11-07 06:31:27 -0300

    fix: card with new middle text

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,6 +1,8 @@
 function Card() {
   return (
-    <div className="lg:w-auto xl:w-[300px] h-[200px] bg-gray-400 rounded-[20px]" />
+    <div className="lg:w-auto xl:w-[300px] h-[200px] bg-gray-400 rounded-[20px] flex items-center justify-center">
+      <span>Card</span>
+    </div>
   );
 }
 
